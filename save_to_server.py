948a0dee40fb52f6d491ded42693ec8bb817b5e2
T.J. Schiffer <tjschiffer@gmail.com>
2018-07-20 19:43:10 +0000

    refactor into own module and handle error states

---
+++ save_to_server.py
@@ -0,0 +1,29 @@
+import requests
+import datetime
+import config.config as config
+from urllib3.connection import NewConnectionError, ConnectTimeoutError
+from urllib3.exceptions import MaxRetryError
+
+
+def save_to_server(data):
+    try:
+        s = requests.session()
+        login = s.post(config.DATABASE_CONFIG['host'] + '/api/login', {
+            'username': config.DATABASE_CONFIG['user'],
+            'password': config.DATABASE_CONFIG['password']
+        })
+        print(login.text)
+
+        save_values = s.post(config.DATABASE_CONFIG['host'] + '/api/insert', data)
+        print(save_values.text)
+    except (NewConnectionError, MaxRetryError, ConnectTimeoutError) as e:
+        return False
+    except:
+        return False
+
+
+save_to_server({
+    'sensor_id': 1,
+    'timestamp': datetime.datetime.utcnow(),
+    'value': 22.5
+})
