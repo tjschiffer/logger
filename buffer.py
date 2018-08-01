f780f377797c5ccf7b66ef145a952115bf5829f1
Unknown <tjschiffer@gmail.com>
2018-07-31 21:41:39 -0400

    add clear method and us os file exists

--- buffer.py
+++ buffer.py
@@ -1,19 +1,24 @@
 import json
+import os
 
 BUFFER_FILENAME = 'buffer.json'
 
 
 def read_buffer():
-    try:
+    if os.path.isfile(BUFFER_FILENAME):
         f = open(BUFFER_FILENAME, 'r', encoding='utf-8')
         json_text = f.read()
         if json_text:
             return json.loads(json_text)
-    except FileNotFoundError:
+    else:
         return []
-    return []
 
 
 def write_buffer(data):
     f = open(BUFFER_FILENAME, 'w', encoding='utf-8')
-    f.write(json.dumps(data, separators=(',', ':')))
+    f.write(json.dumps(data, indent=4, sort_keys=True))
+
+
+def clear():
+    if os.path.isfile(BUFFER_FILENAME):
+        os.remove(BUFFER_FILENAME)
