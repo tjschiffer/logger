40ecdee291205ce843971aa3f109233412e1faf6
Unknown <tjschiffer@gmail.com>
2018-07-31 21:42:26 -0400

    complete save to server

--- save_to_server.py
+++ save_to_server.py
@@ -1,21 +1,26 @@
 import requests
 import config.config as config
 from urllib3.connection import NewConnectionError, ConnectTimeoutError
-from urllib3.exceptions import MaxRetryError
+from urllib3.exceptions import MaxRetryError, ReadTimeoutError
+from requests.exceptions import ReadTimeout
+import json
 
 
 def save_to_server(data):
     try:
         s = requests.session()
-        login = s.post(config.DATABASE_CONFIG['host'] + '/api/login', {
+        # Login and save to session
+        s.post(config.DATABASE_CONFIG['host'] + '/api/login', {
             'username': config.DATABASE_CONFIG['user'],
             'password': config.DATABASE_CONFIG['password']
-        })
-        print(login.text)
-
-        save_values = s.post(config.DATABASE_CONFIG['host'] + '/api/insert', data)
-        print(save_values.text)
-    except (NewConnectionError, MaxRetryError, ConnectTimeoutError) as e:
-        return False
-    except:
+        }, timeout=1)
+        # Send the values
+        save_values = s.put(config.DATABASE_CONFIG['host'] + '/api/insert', json=data)
+        if save_values.status_code != requests.codes.ok or not save_values.text:
+            return False
+        response_data = json.loads(save_values.text)
+        if not response_data['success'] or not response_data['insertedRows']:
+            return False
+        return response_data['insertedRows']
+    except (NewConnectionError, MaxRetryError, ConnectTimeoutError, ReadTimeoutError, ReadTimeout) as e:
         return False
