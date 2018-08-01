934052cc3addedfa8db8af92dec0ebd9582e7562
Unknown <tjschiffer@gmail.com>
2018-07-31 21:42:03 -0400

    fix issue with string key

--- compression.py
+++ compression.py
@@ -6,7 +6,7 @@ RECENT_VALUES_FILENAME = 'recent_values.json'
 
 def save_recent_values(data):
     f = open(RECENT_VALUES_FILENAME, 'w', encoding='utf-8')
-    f.write(json.dumps(data, separators=(',', ':')))
+    f.write(json.dumps(data))
 
 
 def compress_values(values):
@@ -26,13 +26,12 @@ def compress_values(values):
                         abs(recent_values[sensor_id_str] - data_value) > config.DATA_COMPRESSION[sensor_id]:
                     # The value is not within compression or not available
                     compressed_values.append(value)
-                    recent_values[sensor_id] = data_value
+                    recent_values[sensor_id_str] = data_value
         save_recent_values(recent_values)
         return compressed_values
     except FileNotFoundError:
+        # If there is not compression file, save the current values and return them
         for value in values:
             recent_values[value['sensor_id']] = value['value']
         save_recent_values(recent_values)
         return values
-    # except:
-    #     raise ValueError('Sensor id must be an int and value must be a number.')
