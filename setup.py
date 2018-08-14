a7d308c48d3259023b8bd7b2c355fb02d151f607
Unknown <tjschiffer@gmail.com>
2018-08-13 21:44:30 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='logger',
+    version='0.1dev',
+    packages=['logger',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
