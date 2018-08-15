37f998155ea3708a47ff4d5c70080c84a1519713
Unknown <tjschiffer@gmail.com>
2018-08-14 20:16:38 -0400

    repackage for pip

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='logger',
-    version='0.1dev',
-    packages=['logger',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="logger",
+    version="0.1.dev1",
+    author="T.J. Schiffer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tjschiffer/logger",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
